f157c80f1d4903f4cb595e3039b0e39c16b7a2cd
giorgip62 <giorgiop62@gmail.com>
2022-11-07 17:39:16 +0100

    inserita la rubrica

--- java/script.js
+++ java/script.js
@@ -4,14 +4,76 @@ createApp({
   data(){
     return {
 
-      contatti: [{
+      contatti: [
+        {
 
         nome:'Michele',
         avatar:'img/avatar_1.jpg',
         testo: 'Ultimo messaggio inviato',
         orario: '12'
 
-      }]
+        },
+
+        {
+
+          nome:'Fabio',
+          avatar:'img/avatar_2.jpg',
+          testo: 'Ultimo messaggio inviato',
+          orario: '12'
+  
+          },
+
+          {
+
+            nome:'Samuele',
+            avatar:'img/avatar_3.jpg',
+            testo: 'Ultimo messaggio inviato',
+            orario: '12'
+    
+            },
+
+            {
+
+              nome:'Alessandro B',
+              avatar:'img/avatar_4.jpg',
+              testo: 'Ultimo messaggio inviato',
+              orario: '12'
+      
+              },
+
+              {
+
+                nome:'Claudia',
+                avatar:'img/avatar_5.jpg',
+                testo: 'Ultimo messaggio inviato',
+                orario: '12'
+        
+                },
+
+                {
+
+                  nome:'Federico',
+                  avatar:'img/avatar_6.jpg',
+                  testo: 'Ultimo messaggio inviato',
+                  orario: '12'
+          
+                  },
+
+                  {
+
+                    nome:'Davide',
+                    avatar:'img/avatar_7.jpg',
+                    testo: 'Ultimo messaggio inviato',
+                    orario: '12'
+            
+                    },
+
+
+
+
+
+      
+    ]
 
     }
 
